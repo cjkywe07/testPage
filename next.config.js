d03ac8c3d73e713afb9f67d8c4cee9662d2f4e8f
cjkywe07 <jiwony99@gmail.com>
2024-07-22 21:15:36 +0900

    test page 3

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: "export",
+    output: "export",
     reactStrictMode: true,
-    // assetPrefix: process.env.NODE_ENV === "production" ? "https://cjkywe07.github.io/testPage" : "", // production 일때 prefix 경로
+    assetPrefix: process.env.NODE_ENV === "production" ? "https://cjkywe07.github.io/testPage" : "", // production 일때 prefix 경로
     trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
 };
 
