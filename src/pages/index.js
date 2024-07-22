55734e5227186766257efef53be9a1be3381f6e4
cjkywe07 <jiwony99@gmail.com>
2024-07-22 22:47:59 +0900

    test page 6

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import PortfolioContext from "@/content/content";
+import { Img } from "@/styles/main.styles";
 import { useRouter } from "next/router";
 import { useContext } from "react";
 
@@ -15,9 +16,10 @@ const Home = () => {
             <p>메인화면입니다</p>
             <button onClick={onBoardListMoveClick}>게시글 목록 이동</button>
             {/* <div style={{ width: "400px", height: "300px", background: "url(/night.jpg) no-repeat center / contain" }}></div> */}
-            <div
+            {/* <div
                 style={{ width: "400px", height: "300px", background: `url(${prefix}/sea.jpg) no-repeat center / contain` }}
-            ></div>
+            ></div> */}
+            <Img prefix={prefix}></Img>
         </>
     );
 };
